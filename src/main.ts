d0fd995ff5060f4c13ec4e5cb3693be9d697521f
Eugene Zhdanov <euegenezhdanov1@gmail.com>
2025-02-10 05:47:57 +0200

    chore

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { UserModule } from './user/user.module';
 
 async function bootstrap() {
   const user = await NestFactory.create(UserModule);
+
+  user.enableCors({
+    origin: /^https:\/\/api\.rush-up-backend\.com(:\d+)?$/,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
+
   await user.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
