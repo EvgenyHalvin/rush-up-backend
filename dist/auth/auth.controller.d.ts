e53f5edc97c603fd318c4458ab557d0f1c235418
Eugene Zhdanov <euegenezhdanov1@gmail.com>
2025-02-10 04:35:35 +0200

    Remove dist from repository and ignore it

--- dist/auth/auth.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthService } from './auth.service';
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    login(telegramId: string): Promise<{
-        message: string;
-        token: string;
-    }>;
-}
